9b8dc849b1169c9b835fcd44a7b29980b186f55b
Taha Akçin <muhammettaha.akcin@yandex.com>
2024-07-30 16:55:16 +0300

    Exceptions have been defined

--- contact/Controllers/HomeController.cs
+++ contact/Controllers/HomeController.cs
@@ -8,16 +8,36 @@ using Microsoft.AspNetCore.DataProtection;
 using System.Configuration;
 using static StartUp.StartUp;
 using Microsoft.Extensions.Options;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 
 
 namespace contact.Controllers
 {
+    public class NameNotProvidedException : Exception
+    {
+        public NameNotProvidedException() : base("Name field cannot be left blank.") { }
+        public NameNotProvidedException(string message) : base(message) { }
+        public NameNotProvidedException(string message, Exception inner) : base(message, inner) { }
+    }
+    public class EmailNotProvidedException : Exception
+    {
+        public EmailNotProvidedException() : base("Email field cannot be left blank.") { }
+        public EmailNotProvidedException(string message) : base(message) { }
+        public EmailNotProvidedException(string message, Exception inner) : base(message, inner) { }
+    }
+
     public class HomeController : Controller
     {
 
         readonly IConfiguration configuration;
 
+        
+        
         public HomeController(IConfiguration configuration)
         {
             this.configuration = configuration; 
@@ -34,6 +54,8 @@ namespace contact.Controllers
             return View();
         }
 
+        
+
         public ActionResult Contact()
         {
             
@@ -46,6 +68,10 @@ namespace contact.Controllers
             var myPassword = configuration["MailInfo:Password"];
             try
             {
+                if (name == null) { throw new NameNotProvidedException(); }
+                if (email == null) { throw new EmailNotProvidedException(); }
+                if (message == null) {}
+                
                 MailMessage mail = new MailMessage();
                 SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
                 
@@ -62,13 +88,23 @@ namespace contact.Controllers
 
                 smtpClient.Send(mail);
                 ViewBag.Message = "Your message has been sent successfully!";
+            }
 
+            catch (NameNotProvidedException ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+
+            catch (EmailNotProvidedException ex)
+            {
+                ViewBag.Message = ex.Message;
             }
 
             catch (Exception ex)
             {
                 ViewBag.Message = "Error: " +  ex.Message;
             }
+            
 
             return View();
         }
---
+++ contact/StaticFiles/script.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sendButton = document.getElementById("send-button");
+    const textArea = document.getElementById("text-area"); 
+
+    sendButton.addEventListener("click", emptyAlert);
+
+    function emptyAlert(event) {
+        // event.preventDefault();
+        
+        if (textArea == null || textArea.value.trim() == "") {
+            alert("Text area cannot be empty.");
+            return
+        }
+
+        document.getElementById("contact-form").submit();
+    }
+})
+
+
+
